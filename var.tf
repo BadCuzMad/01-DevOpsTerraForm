c8a7e220366a2ec50207d700bb300f6895da3da9
BadCuzMad <mme37005@gmail.com>
2023-01-09 17:10:06 +0400

    first terraform version

---
+++ var.tf
@@ -0,0 +1,18 @@
+variable "aws_access_key" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "aws_session_token" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
