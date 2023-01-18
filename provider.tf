31c87da410fd0b07a4c3abdf65c962eec8b06878
BadCuzMad <mme37005@gmail.com>
2023-01-18 22:13:26 +0400

    secrets

--- provider.tf
+++ provider.tf
@@ -15,11 +15,11 @@ terraform {
 
 
 provider "aws" {
-  region  = "us-east-1"
-  profile = "demo_acc"
+  region = "us-east-1"
+  //profile = "demo_acc"
   //shared_config_files      = ["%USERPROFILE%\\.aws\\config"]
   //shared_credentials_files = ["%USERPROFILE%\\.aws\\credentials"]
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-  token      = var.aws_session_token
+  // access_key = var.aws_access_key_id
+  // secret_key = var.aws_secret_access_key
+  // token      = var.aws_session_token
 }
