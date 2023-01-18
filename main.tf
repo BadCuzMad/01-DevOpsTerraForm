ed9eb05c82faf0a7662234ffe62ecce02050238a
BadCuzMad <mme37005@gmail.com>
2023-01-18 21:58:40 +0400

    secrets

--- main.tf
+++ main.tf
@@ -37,11 +37,11 @@ locals {
     "exit 100;",
     "fi;",
     "done;",
-  "echo \"debug 4 finish\"",
-  "docker stop ipsec-vpn-server",
-  "docker rm ipsec-vpn-server",
-  "docker run --name ipsec-vpn-server --restart=always -v ikev2-vpn-data:/etc/ipsec.d -v /lib/modules:/lib/modules:ro -p 500:500/udp -p 4500:4500/udp -d --privileged hwdsl2/ipsec-vpn-server",
-  "sleep 15",
+    "echo \"debug 4 finish\"",
+    "docker stop ipsec-vpn-server",
+    "docker rm ipsec-vpn-server",
+    "docker run --name ipsec-vpn-server --restart=always -v ikev2-vpn-data:/etc/ipsec.d -v /lib/modules:/lib/modules:ro -p 500:500/udp -p 4500:4500/udp -d --privileged hwdsl2/ipsec-vpn-server",
+    "sleep 15",
   "docker logs ipsec-vpn-server"]
 }
 
--- provider.tf
+++ provider.tf
@@ -5,13 +5,20 @@ terraform {
       version = "~> 4.16"
     }
   }
-
+  backend "s3" {
+    bucket = "knm-delete-me"
+    key    = "terraform-state"
+    region = "us-east-1"
+  }
   required_version = ">= 1.2.0"
 }
 
 
 provider "aws" {
-  region     = "us-east-1"
+  region                   = "us-east-1"
+  profile                  = "demo_acc"
+  //shared_config_files      = ["%USERPROFILE%\\.aws\\config"]
+  //shared_credentials_files = ["%USERPROFILE%\\.aws\\credentials"]
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
   token      = var.aws_session_token
